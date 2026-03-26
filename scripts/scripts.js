30bb61ac62acb8d2c6717138aebf0f9ef937297d
cowpatchmedia <cowpatchmedia@gmail.com>
2026-03-25 17:09:20 -0700

    create js folder and css folder and organize files

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+// behold widget script. This is used to load the behold widget for the instagram feed. 
+(() => {
+    const d=document,s=d.createElement("script");s.type="module";
+    s.src="https://w.behold.so/widget.js";d.head.append(s);
+})();
